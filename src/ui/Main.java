7c95cdd1ad4d7ff2f4c31b8cf1a4a7fd588a3cf0
unknown <camilo_aea@hotmail.com>
2019-04-01 17:57:26 -0500

    Mi primer repositorio

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,61 +4,41 @@ import model.*;
 
 import java.util.Scanner;
 
-public class Main {
-
+public class Main{
 	
-	public static void Main(String[] args) throws Exception{
+	public static void main (String[] args){
+	
+	Scanner reader = new Scanner(System.in);
 
-		Scanner reader = new Scanner(System.in);
+		int option = 0;
+		
 
-		int option;
-		cleanScreenConsole();
-
-		do {
+		while(option=1) {
 
 		    System.out.println("Bienvenido a la verinaria");
 		    System.out.println("Por favor seleccione la opcion que desea:");
-			System.out.println("1.Registrar dueño.");
-			System.out.println("2.Registrar mascota.");
+			System.out.println("1.Registrar Mascota.");
+			System.out.println("2.Registrar Dueño.");
 			System.out.println("3.Ver clientes.");
-			System.out.println("4.Ver mascotas.");
+			System.out.println("4.Mostrar la disponbilidad de los cuertos.");
 			System.out.println("5.Creacion de historia clinica a mascota hopitalizada.");
-			System.out.println("6.Informe clinico de las historia clinica del paciente.");
-			System.out.println("7.Consultar datos del dueño de el animal hospitalizado con el id del dueño.");
-			System.out.println("8.");
-			System.out.println("9.");
-			System.out.println("10.");
-			System.out.println("11.");
-			System.out.println("12.");
-			System.out.println("13.");
-			System.out.println("14.");
-			System.out.println("15.");	
+			System.out.println("6.Informe de oaciente hospitalizados en el momento.");
+			System.out.println("7.Consultar datos del dueño de el animal hospitalizado con el nombre del dueño.");
+			System.out.println("8.Consultar datos del dueño de el animal hospitalizado con el nombre de la mascota.");
+			System.out.println("9.Dar de alta a una mascota hopitalizada.");
+			System.out.println("10.Calcular costos de hospitalización.");
+			System.out.println("11.Calcular costo de medicamento.");
+			System.out.println("12.Calcular la cantidad de ingresos por concepto de hospitalizacion a un mascota.");
+			System.out.println("13.Permitir el minicuarto en donde esta alojada la mascota con su nombre.");
+			System.out.println("14.Calcular ingresos de la veterinaria por hospitalizacion.");
+			System.out.println("15.Salir.");
+				
 
 			option = Integer.parseInt(reader.nextLine());
 
-			cleanScreenConsole();
+		
 
-			switch(option){
-
-				case 1:
-
-					System.out.println("adadca");	
-
-				break;
-
-			}
-
-		} while(option!=15);
-
-	
-	}	
-
-	public static void cleanScreenConsole(){
-		//Try clean the console screen
-		try{
-			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-		}catch(Exception ex){
-			//nothing to do
-		}			
+			
+		} 
 	}
 }
